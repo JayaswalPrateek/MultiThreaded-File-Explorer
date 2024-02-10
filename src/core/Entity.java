cafa6187f46035d50b13a3f1e67954846a1048ef
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-02-10 19:03:00 +0530

    feat: extend Runnable Interface, add varargs support

---
+++ src/core/Entity.java
@@ -0,0 +1,39 @@
+package core;
+
+interface Entity extends Runnable {
+
+    ErrorCode create(final String... names);
+
+    ErrorCode create(final String destination, final String... names);
+
+    ErrorCode copy(final String destination);
+
+    ErrorCode copy(final String newName, final String destination);
+
+    ErrorCode delete(final String... names);
+
+    ErrorCode delete(final String destination, final String... names);
+
+    ErrorCode move(final String destination);
+
+    ErrorCode move(final String destination, final String... newName);
+
+    ErrorCode rename(final String newName);
+
+}
+
+interface File extends Entity {
+    ErrorCode open();
+
+    ErrorCode properties();
+}
+
+interface Folder extends Entity {
+    ErrorCode list();
+
+    ErrorCode regexFilter(final String pattern);
+
+    ErrorCode stepIn(final String target);
+
+    ErrorCode stepOut();
+}
