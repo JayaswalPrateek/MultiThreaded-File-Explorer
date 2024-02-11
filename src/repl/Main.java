5ca1ee8fd5790e8b9a6744c7306d87c168e7c54b
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-02-11 11:49:41 +0530

    feat: add REPL package boilerplate

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,8 +1,11 @@
-package ui;
+package repl; // Read-Evaluate-Print Loop
 
 import core.*;
 
 public class Main {
     public static void main(final String[] args) {
+        while (true) {
+
+        }
     }
 }
