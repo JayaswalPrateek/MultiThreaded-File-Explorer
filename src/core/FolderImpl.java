4ef9c55edf50abdbc89fb55edb5599a2e1a2a515
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-02-13 21:44:04 +0530

    fix: remove incorrect @override annotations

--- src/core/FolderImpl.java
+++ src/core/FolderImpl.java
@@ -36,91 +36,76 @@ public final class FolderImpl implements Folder {
         return getPath() + getName();
     }
 
-    @Override
     public ErrorCode create(final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
-    @Override
     public ErrorCode create(final String destination, final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
-    @Override
     public ErrorCode copy(final String destination) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'copy'");
     }
 
-    @Override
     public ErrorCode copy(final String destination, final String... newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'copy'");
     }
 
-    @Override
     public ErrorCode delete(final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
-    @Override
     public ErrorCode delete(final String destination, final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
-    @Override
     public ErrorCode move(final String destination) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
-    @Override
     public ErrorCode move(final String destination, final String... newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
-    @Override
     public ErrorCode rename(final String newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'rename'");
     }
 
-    @Override
     public void run() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 
-    @Override
     public CopyOnWriteArrayList<String> listFolders() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listFolders'");
     }
 
-    @Override
     public CopyOnWriteArrayList<String> listFiles() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listFiles'");
     }
 
-    @Override
     public ErrorCode regexFilter(final String pattern) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'regexFilter'");
     }
 
-    @Override
     public ErrorCode stepIn(final String target) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'stepIn'");
     }
 
-    @Override
     public ErrorCode stepOut() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'stepOut'");
