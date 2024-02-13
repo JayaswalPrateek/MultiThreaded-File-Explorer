4863b164ed45dd53e8ab34c0ed2ae1353dd86c9a
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-02-13 21:41:58 +0530

    fix: remove incorrect @override annotations

--- src/core/FileImpl.java
+++ src/core/FileImpl.java
@@ -34,73 +34,61 @@ public final class FileImpl implements File {
         return getPath() + getName();
     }
 
-    @Override
     public ErrorCode create(final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
-    @Override
     public ErrorCode create(final String destination, final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
-    @Override
     public ErrorCode copy(final String destination) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'copy'");
     }
 
-    @Override
     public ErrorCode copy(final String destination, final String... newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'copy'");
     }
 
-    @Override
     public ErrorCode delete(final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
-    @Override
     public ErrorCode delete(final String destination, final String... names) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
-    @Override
     public ErrorCode move(final String destination) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
-    @Override
     public ErrorCode move(final String destination, final String... newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
-    @Override
     public ErrorCode rename(final String newName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'rename'");
     }
 
-    @Override
     public void run() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 
-    @Override
     public ErrorCode open() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'open'");
     }
 
-    @Override
     public ErrorCode properties() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'properties'");
