9b69512a1ea04f3c9e335baf52acfa506c9eed56
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-03-19 18:56:38 +0530

    feat: add createLargeFile.sh script to create 2 large files for demo purposes

---
+++ createLargeFile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+create_large_file() {
+    filename=$1
+    size_gb=$2
+    # 1 GB = 1024 * 1024 * 1024 bytes
+    size_bytes=$((size_gb * 1024 * 1024 * 1024))
+
+    # Create the file with the desired size using dd command
+    dd if=/dev/zero of=$filename bs=1G count=$size_gb &> /dev/null
+}
+
+# Main script
+filename1="large_file1.dat"
+filename2="large_file2.dat"
+size_gb=5  # Change this to the desired size in gigabytes
+create_large_file $filename1 $size_gb
+echo "Large file '$filename1' created with size $size_gb GB."
+
+create_large_file $filename2 $size_gb
+echo "Large file '$filename2' created with size $size_gb GB."
